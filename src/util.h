65c1a3e4375e6149b640d586a449f3041af4e3f1
Fynn Raddatz <fynn.raddatz@gmail.com>
2024-05-29 22:32:07 +0200

    stop using 0x prefix until axis spacing is fixed

--- src/util.h
+++ src/util.h
@@ -5,7 +5,7 @@
 std::string number_to_hex(int num)
 {
     std::stringstream ss;
-    ss << "0x" << std::setw(2) << std::setfill('0') << std::uppercase << std::hex << num;
+    ss << "" << std::setw(2) << std::setfill('0') << std::uppercase << std::hex << num;
     return ss.str();
 }
 
