590513a1917436413ac0b913595bb7bc93d22b40
Fynn Raddatz <fynn.raddatz@gmail.com>
2024-05-30 17:55:17 +0200

    changed settings from defines to constexpr

--- src/hexamine_config.h
+++ src/hexamine_config.h
@@ -1,10 +1,10 @@
 #ifndef HEXAMINE_CONFIG_H
 #define HEXAMINE_CONFIG_H
 
-#define HEXAMINE_VERSION "v2.1"
+constexpr const char* HEXAMINE_VERSION = "v2.1";
 
-#define HEXAMINE_MAX_FPS 60
-#define HEXAMINE_VSYNC true
-#define HEXAMINE_ANTIALIASING_LEVEL 16
+constexpr unsigned int HEXAMINE_MAX_FPS = 60;
+constexpr bool HEXAMINE_VSYNC = true;
+constexpr unsigned int HEXAMINE_ANTIALIASING_LEVEL = 16;
 
 #endif // HEXAMINE_CONFIG_H
